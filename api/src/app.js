e9dbee6407f2f692f3bfa08620f3acaf35c27c36
Anuarder <anuar.ibraev97@gmail.com>
2018-12-29 21:09:02 +0600

    setup database

--- api/src/app.js
+++ api/src/app.js
@@ -2,19 +2,18 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const config = require('./config/config');
+const mongoose = require('mongoose');
 
+mongoose.connect('mongodb://localhost/AsianFood');
 const app = express();
 
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(cors());
 
-let port = process.env.PORT || 8081;
 
-app.get('/status', (req, res) => {
-    res.send({
-        message: 'hello world'
-    })
-})
+require('./routes/index')(app);
 
-app.listen(port);
+
+app.listen(config.port);
