3ef51ca72d1cb2969a99bb7f81effe610797ac2d
Anuarder <anuar.ibraev97@gmail.com>
2018-12-31 16:23:01 +0600

    Setup frontend

--- template/src/views/Home.vue
+++ template/src/views/Home.vue
@@ -1,13 +1,9 @@
 <template>
-  <HelloWorld />
+  <v-container>
+    <v-layout>
+      <v-flex xs12>
+        <h1 class="text-xs-center">Home page</h1>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
-
-<script>
-  import HelloWorld from '../components/HelloWorld'
-
-  export default {
-    components: {
-      HelloWorld
-    }
-  }
-</script>
